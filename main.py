5eac713cb34ec8f360b5c9e75528101848b2e40e
Michael Wallerius <mikesterciv1@gmail.com>
2021-01-02 09:41:03 +0000

    separated dog by breed and random dog images into two separate functions. still trying to get the dog by breed with spaces to work correctly

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from keep_alive import keep_alive
 
 client = discord.Client()
 
-def get_dogGIF(breed):
-  url = "https://dog.ceo/api/breed/"+str(breed)+"/images/random"
+def get_randomDog():
+  url = "https://dog.ceo/api/breeds/image/random"
   response = requests.get(url)
   json_data = json.loads(response.text)
+  print(json_data['message'])
   return json_data['message']
+
+def get_dogByBreed(variation, breed):
+  url = "https://dog.ceo/api/breed/"+breed+"/images"
+  print(url)
+  response = requests.get(url)
+  json_data = json.loads(response.text)
+
+  #create a separate list from the main list of just the variations of the breed that we want
+  #maybe try first and last index of the breed then just use random to pick and index for a random photo
+
+  return json_data['message'][1]
 
 def get_catGIF():
   response = requests.get("https://api.thecatapi.com/v1/images/search")
@@ -31,9 +43,16 @@ async def on_message(message):
     return
 
   if msg.startswith("!dog"):
-    breed = msg.split("!dog ")[1]
-    await message.channel.send(get_dogGIF(breed))
-    print (breed)
+    split_msg = msg.split("!dog ")[1]
+    if "random" in split_msg:
+      print(True)
+      await message.channel.send(get_randomDog())
+      return
+    breed = split_msg.split(" ")
+    first_half_breed = breed[0]
+    second_half_breed = breed[1]
+    await message.channel.send(get_dogByBreed(first_half_breed,second_half_breed))
+
 
   if msg.startswith('cat please'):
     await message.channel.send(get_catGIF())
