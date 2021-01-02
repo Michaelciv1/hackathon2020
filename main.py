15ce8a3f14056af10b4cc073f8d5ff8bf577220d
Michael Wallerius <mikesterciv1@gmail.com>
2021-01-02 08:44:29 +0000

    !dog now allows for specifying breed. cannot yet do multiword dog breeds though

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import os
 from keep_alive import keep_alive
 
 client = discord.Client()
-print(os.getenv("TOKEN"))
 
-def get_dogGIF():
-  response = requests.get("https://dog.ceo/api/breeds/image/random")
+def get_dogGIF(breed):
+  url = "https://dog.ceo/api/breed/"+str(breed)+"/images/random"
+  response = requests.get(url)
   json_data = json.loads(response.text)
   return json_data['message']
 
@@ -18,30 +18,32 @@ def get_catGIF():
   json_data = json.loads(response.text)
   return json_data[0]['url']
 
-def encourage():
-  response_list = ["No balls you won't", "I hope you do", "Good"]
-  return random.choice(response_list)
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-      return
+  msg = message.content
+  sender = message.author
 
-    if message.content.startswith('hello'):
-      await message.channel.send('Hello!')
+  if sender == client.user:
+    return
 
-    if message.content.startswith('dog please'):
-      await message.channel.send(get_dogGIF())
+  if msg.startswith("!dog"):
+    breed = msg.split("!dog ")[1]
+    await message.channel.send(get_dogGIF(breed))
+    print (breed)
 
-    if message.content.startswith('cat please'):
-      await message.channel.send(get_catGIF())
+  if msg.startswith('cat please'):
+    await message.channel.send(get_catGIF())
 
-    if message.content.startswith('I wanna kill myself'):
-      await message.channel.send(encourage())
+  if msg.startswith('WAP please'):
+    await message.channel.send(get_catGIF())
+
+  if msg.startswith('money please'):
+    await message.channel.send(file=discord.File('./images/moneyman.jpeg'))
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
